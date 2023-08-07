0d58e06f9381e3e6eb96d47a3d1201339891ae32
Alexandr <dunkelpunk@gmail.com>
2023-08-07 19:50:01 +0300

    домашнее задание ооп

---
+++ task4/DataContainer.java
@@ -0,0 +1,31 @@
+package OOP4.task4;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class DataContainer {
+    private List<T> data;
+
+    public DataContainer(){
+        data = new ArrayList<>();
+    }
+
+    public void add(T element){
+        data.add(element);
+    }
+
+    public T get(int index){
+        return this.data.get(index);
+    }
+
+    public void sort(Comparator<T> comparator){
+        data.sort(comparator);
+    }
+
+    public void print(){
+        for (T element : data){
+            System.out.println(element);
+        }
+    }
+}
